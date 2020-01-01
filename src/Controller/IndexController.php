24fbada24f9edd3f9d9837013769e04a3e917551
Karol Dabrowski <kontakt@karoldabrowski.pl>
2020-01-01 13:28:17 +0100

    Create an empty main page

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+	/**
+	 * @Route("/")
+	 */
+	public function index()
+	{
+		return $this->render("index/index.html.twig");
+	}
+}
