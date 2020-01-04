fc8331fea8bbbfbd46abe564c3bc0452952f6731
Karol Dabrowski <kontakt@karoldabrowski.pl>
2020-01-04 18:42:19 +0100

    Create NoteController and boilerplate method for deleting notes

---
+++ src/Controller/NoteController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NoteController extends AbstractController
+{
+	/**
+	 * @Route("/note/{noteId}", methods={"POST"}, requirements={"noteId"="\d+"})
+	 */
+	public function delete($noteId)
+	{
+		dd($noteId);
+		//TODO Delete note with id $noteId
+	}
+}
