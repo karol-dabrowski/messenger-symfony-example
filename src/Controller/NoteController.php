87a63fa19675ab7c35cdaaac0cd8d4f4827d8af7
Karol Dabrowski <kontakt@karoldabrowski.pl>
2020-01-04 19:44:41 +0100

    Create template method for adding notes

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -3,9 +3,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class NoteController extends AbstractController
 {
+	/**
+	 * @Route("/note/add", methods={"POST"})
+	 */
+	public function add(Request $request)
+	{
+		$noteTitle = $request->get("noteTitle");
+		$noteDescription = $request->get("noteDescription");
+		dd($noteTitle, $noteDescription);
+		//TODO Add new note
+	}
+
 	/**
 	 * @Route("/note/delete/{noteId}", methods={"POST"}, requirements={"noteId"="\d+"})
 	 */
