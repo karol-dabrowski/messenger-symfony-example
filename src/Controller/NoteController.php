107692a761cfd9e3cd9a754e2ba6424113e23f5e
Karol Dabrowski <kontakt@karoldabrowski.pl>
2020-02-09 15:01:27 +0100

    Implement command dispatching in Note controller

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -1,21 +1,35 @@
 <?php
 namespace App\Controller;
 
+use App\Command\AddNote;
+use App\Query\GetSomething;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class NoteController extends AbstractController
 {
 	/**
 	 * @Route("/note/add", methods={"POST"})
 	 */
-	public function add(Request $request)
+	public function add(Request $request, MessageBusInterface $commandBus)
 	{
+		$noteId = Uuid::uuid4();
 		$noteTitle = $request->get("noteTitle");
 		$noteDescription = $request->get("noteDescription");
-		dd($noteTitle, $noteDescription);
-		//TODO Add new note
+
+		try {
+			$commandBus->dispatch(
+				new AddNote($noteId, $noteTitle, $noteDescription)
+			);
+		} catch (\Exception $e) {
+			//TODO Handle exception
+			dd($e->getMessage());
+		}
+
+		return $this->redirectToRoute('index');
 	}
 
 	/**
