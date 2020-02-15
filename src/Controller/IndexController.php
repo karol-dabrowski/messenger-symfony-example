304b90a98d18b50c38b6ae566fced3de0ff814f6
Karol Dabrowski <kontakt@karoldabrowski.pl>
2020-02-15 19:00:28 +0100

    Implement index action in Index controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Controller;
 
+use App\Query\GetAllNotes;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -9,10 +12,11 @@ class IndexController extends AbstractController
 	/**
 	 * @Route("/", name="index")
 	 */
-	public function index()
+	public function index(MessageBusInterface $queryBus)
 	{
-		//TODO Implement retrieving data
-		$notes = [];
+		$envelope = $queryBus->dispatch(new GetAllNotes());
+		$handledStamp = $envelope->last(HandledStamp::class);
+		$notes = $handledStamp->getResult();
 
 		return $this->render("index/index.html.twig", ["notes" => $notes]);
 	}
