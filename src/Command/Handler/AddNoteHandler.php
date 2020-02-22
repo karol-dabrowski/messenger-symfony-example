8b6605b5a46df69743bdc76f3f4bb49782a58ded
Karol Dabrowski <kontakt@karoldabrowski.pl>
2020-02-22 20:11:11 +0100

    Implement dispatching NoteCreated event

--- src/Command/Handler/AddNoteHandler.php
+++ src/Command/Handler/AddNoteHandler.php
@@ -3,19 +3,28 @@ namespace App\Command\Handler;
 
 use App\Command\AddNote;
 use App\Entity\Note;
+use App\Event\NoteCreated;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DispatchAfterCurrentBusStamp;
 
 class AddNoteHandler implements MessageHandlerInterface
 {
 	private $notes;
 	private $em;
+	private $eventBus;
 
-	public function __construct(NoteRepository $noteRepository, EntityManagerInterface $em)
-	{
+	public function __construct(
+		NoteRepository $noteRepository,
+		EntityManagerInterface $em,
+		MessageBusInterface $eventBus
+	) {
 		$this->notes = $noteRepository;
 		$this->em = $em;
+		$this->eventBus = $eventBus;
 	}
 
 	public function __invoke(AddNote $command)
@@ -34,5 +43,9 @@ class AddNoteHandler implements MessageHandlerInterface
 
 		$this->em->persist($note);
 		$this->em->flush();
+
+		$event = new NoteCreated($noteId);
+		$envelope = new Envelope($event);
+		$this->eventBus->dispatch($envelope->with(new DispatchAfterCurrentBusStamp()));
 	}
 }
